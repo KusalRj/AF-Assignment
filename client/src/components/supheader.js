ebe0b505e966877005ab48af7ff6c872944874cc
Kusal Rajarathne <90107092+kusalr2@users.noreply.github.com>
2022-06-04 16:23:37 +0530

    main

--- client/src/components/supheader.js
+++ client/src/components/supheader.js
@@ -2,9 +2,10 @@ import React from "react";
 
 function Header(){
     return(
+        
         <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
-            <a className="navbar-brand" href="/">Navbar</a>
+            <a className="navbar-brand" href="/">Supervisor</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
@@ -25,6 +26,16 @@ function Header(){
                 <li className="nav-item">
                 <a className="nav-link" href="/chat">ChatBox</a>
                 </li>
+
+                <li al className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Mr. Lucid 
+          </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a className="dropdown-item" href="#">Sign Out</a></li>
+                
+            </ul>
+            </li>
             </ul>
             </div>
         </div>
