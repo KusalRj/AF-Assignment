82cdedc10e55826f62daec8a5eb9952a7aa3f958
Kusal Rajarathne <90107092+kusalr2@users.noreply.github.com>
2022-06-04 13:19:35 +0530

    commit01

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import { Route, Routes, Navigate } from "react-router-dom";
 import StudentProfile from "./components/StudentProfile";
 import Signup from "./components/Singup/signUp";
@@ -10,6 +11,12 @@ import Home from "./components/Home";
 import Panels from "./components/panels";
 import Staff from "./components/staff";
 import Students from "./components/students";
+import AddStuMarks from './components/addStuMarksSp';
+import Retrivemarks from './components/retrievemarks';
+//import ChatPage from './components/chatpage';
+import Accepttopics from './components/accepttopics';
+import Topicedit from './components/topicviewedit';
+
 //import FileUpload from "./components/FileUpload";
 function App() {
 	const user = localStorage.getItem("token");
@@ -28,6 +35,11 @@ function App() {
             <Route path="/staff" element={<Staff/>}/>
             <Route path="/panels" element = {<Panels/>}/>
         	<Route path="/admin" element = {<Home/>}/>
+			<Route path="/add"  exact element={<AddStuMarks />}></Route>
+          {/*<Route path="/chat"  exact element={<ChatPage />}></Route>*/}
+          <Route path='/view' exact element={<Retrivemarks/>}></Route>
+          <Route path="/topic" exact element={<Accepttopics/>}></Route>
+          <Route path="/topicview" exact element={<Topicedit/>}></Route>
 			{/* <Route path="/fileupload" exact element={< FileUpload/>} /> */}
 
 			
